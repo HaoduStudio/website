56f6986fe83282ec1c04cd559de2876531912c26
MengXi <me@zhunote.cn>
2024-05-04 13:31:20 +0800

    create:chatra program

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,7 +6,24 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "每日手帐",
   description: "DailyNotes操作文档",
-
+  head: [
+    [
+      "script",
+      {},
+      `\
+        (function(d, w, c) {
+            w.ChatraID = 'tAi3YDBi6MnEnWsJ9';
+            var s = d.createElement('script');
+            w[c] = w[c] || function() {
+                (w[c].q = w[c].q || []).push(arguments);
+            };
+            s.async = true;
+            s.src = 'https://call.chatra.io/chatra.js';
+            if (d.head) d.head.appendChild(s);
+        })(document, window, 'Chatra');
+      `,
+    ],
+  ],
   theme,
 
 });
