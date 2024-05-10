418f1e3dc2b99e868f721ce6decea8242b975070
MengXi <me@zhunote.cn>
2024-05-10 22:33:53 +0800

    update and fixed:privacy and gtag

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,6 +8,18 @@ export default defineUserConfig({
   description: "DailyNotes操作文档",
   head: [
     ["script", { src: "//code.tidio.co/ja0zr5nrdcd0fnn6lksr7wi0c6d36848.js" }],
+    ["script", { src: "https://www.googletagmanager.com/gtag/js?id=G-91VRX6N8WK" }],
+    [
+      "script",
+      {},
+      `\
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-91VRX6N8WK');
+      `,
+    ],
   ],
   theme,
 
