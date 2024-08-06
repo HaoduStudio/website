e58bbc530c64fd0350ec8fa055bd01da35c385cf
MengXi <me@zhunote.cn>
2024-08-06 14:35:03 +0800

    New Docs:Astro+Starlight

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: '每日手帐',
+			locales: {
+				root: {
+					label: '简体中文',
+					lang: 'zh-CN',
+				},
+			},
+			social: {
+				github: 'https://github.com/HaoduStudio/website',
+			},
+			sidebar: [
+				{
+					label: '快速上手',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: '安装软件', slug: 'started/index' },
+					],
+				},
+			],
+		}),
+	],
+});
